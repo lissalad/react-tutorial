02ff15cb97611436253d2de591cdeed9f3771240
lissalad <elissalaymon@gmail.com>
2022-02-09 15:52:21 -0800

    dynamic data with json

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import "./POPOSSpace.css";
 
 function POPOSSpace(props) {
-  const { name, image, address } = props
+  const { name, image, address, hours } = props
   return (
     <div className="POPOSSpace">
       <img src={`${process.env.PUBLIC_URL}/images/${image}`}     
@@ -13,7 +13,7 @@ function POPOSSpace(props) {
       />
       <h1>{name}</h1>
       <div>{address}</div>
-
+      <p>{hours}</p>
     </div>
   )
 }
