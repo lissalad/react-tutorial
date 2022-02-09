d6d4c21de2b18c4cad97f06a097cbf0865e9f4d6
lissalad <elissalaymon@gmail.com>
2022-02-08 18:04:27 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+// src/Title.js
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Header">
+      <h1 className='Title'>SFPOPOS</h1>
+      <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  )
+}
+
+export default Title
